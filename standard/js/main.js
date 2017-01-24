f03ed20b717391013bdfcf824534de680f6c98c5
cdmcmill13 <cdmcmill@aggies.ncat.edu>
2017-01-24 17:45:49 -0500

    Delete main.js

--- standard/js/main.js
+++
@@ -1,7 +0,0 @@
-var CrystalHunter = CrystalHunter || {};
-
-// Initiate Phaser
-CrystalHunter.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
-
-CrystalHunter.game.state.add('GameState', CrystalHunter.GameState);
-CrystalHunter.game.state.start('GameState');    
