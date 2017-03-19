b7c87320222440e503696512800ec5e9b49adec2
cdmcmill13 <cdmcmill@aggies.ncat.edu>
2017-03-19 01:16:51 -0400

    Update game.js
    Added settings menu state

--- sample/js/game.js
+++ sample/js/game.js
@@ -11,6 +11,7 @@ game.global = {
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
+game.state.add('settings', settingsState);
 game.state.add('play', playState);
 
 // Start boot state
