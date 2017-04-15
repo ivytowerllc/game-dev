ec9f3f88289fe2cf4a88d7c9cf70aace0ae109a3
Corey McMillan <cdmcmill@aggies.ncat.edu>
2017-04-15 15:51:32 -0400

    Update settings.js

--- sample/js/settings.js
+++ sample/js/settings.js
@@ -6,18 +6,20 @@ var settingsState = {
         this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'bg');
 
         // Display the name of the game
-        var nameLabel = game.add.text(game.width/2, -50, "Settings", {font: '50px Arial', fill: '#ffffff'});
+        var nameLabel = game.add.text(game.width/2, -50, "Settings", {font: '50px Josefin Slab', fill: '#ffffff'});
         nameLabel.anchor.setTo(.5,.5);
-        var nameTween = game.add.tween(nameLabel).to({y: 90}, 1000).easing(Phaser.Easing.Bounce.Out).start();
+        game.add.tween(nameLabel).to({y: 90}, 1000).easing(Phaser.Easing.Bounce.Out).start();
 
         // Background music
         this.music = game.add.audio('asteroidMenu');
         this.music.loop = true;
         this.music.play();
 
-        this.menuButton = game.add.button(50, 50, 'menuButton', this.menu, this);
-        this.menuButton.scale.setTo(.1);
+        this.menuButton = game.add.button(50, 50, 'redButton', this.menu, this);
         this.menuButton.anchor.setTo(.5,.5);
+
+        this.creditsButton = game.add.button(game.width*.95, 50, 'yellowButton', this.credits, this);
+        this.creditsButton.anchor.setTo(.5,.5);
 
     },
 
@@ -27,5 +29,10 @@ var settingsState = {
         game.state.start('menu');
 
     },
+
+    credits: function(){
+        this.music.stop();
+        game.state.start('credits');
+    }
 
 };
