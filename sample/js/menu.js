555a027b039c949378e607b858179a7e41054129
Corey McMillan <cdmcmill@aggies.ncat.edu>
2017-04-30 20:08:13 -0400

    Update menu.js

--- sample/js/menu.js
+++ sample/js/menu.js
@@ -14,22 +14,59 @@ var menuState = {
         this.music.play();
 
         // Display the name of the game
-        var nameLabel = game.add.text(game.width/2, -50, "Crystal Hunter", {font: '50px Josefin Slab', fill: '#ffffff'});
+        var nameLabel = game.add.text(game.width/2, -50, "Crystal Hunter ALPHA TEST", {font: '50px Josefin Slab', fill: '#ffffff'});
         nameLabel.anchor.setTo(.5,.5);
         /** Name Tween **/ game.add.tween(nameLabel).to({y: 90}, 1000).easing(Phaser.Easing.Bounce.Out).start();
 
+        var hints = [
+            "Tip: After collecting a new crystal, press ENTER to switch to a new weapon type!",
+            "Tip: Collect multiple crystals of the same type to upgrade your weapon!",
+            "Tip: Collect anomalies to get temporary buffs!",
+            "Tip: Destroy asteroids and collect dust to grow larger!",
+            "Tip: Collected dust can be used as a shield against attacks!",
+            "Tip: After collecting dust, hold SPACEBAR to boost!",
+            "Tip: Destroy asteroids to collect metals, crystals, and asteroid dust!",
+            "Tip: Destroy enemies to increase your score!"
+        ];
+
+
+
+        // Hints
+        var hintLabel = game.add.text(game.width/2, game.height*.75, hints[(Math.floor(Math.random()*8))], {font: '25px Josefin Slab', fill:'#ffffff'});
+        hintLabel.anchor.setTo(.5,.5);
+
+        var mediaLabel = game.add.text(game.width/2, game.height*.95, "Like the game? Let us know on twitter! @IvyTowerLLC", {font: '20px Josefin Slab', fill:'#ffffff'});
+        mediaLabel.anchor.setTo(.5,.5);
+
         // Show the score at the center of the screen
-        var scoreLabel = game.add.text(game.width/2, game.height/3, 'Score: ' + game.global.score, {font: '25px Josefin Slab', fill: '#ffffff'});
+        var scoreLabel = game.add.text(game.width/2, game.height/1.7 + 50, 'Score: ' + game.global.score, {font: '25px Josefin Slab', fill: '#ffffff'});
         scoreLabel.anchor.setTo(.5,.5);
 
-        this.nickname = game.add.inputField(game.width/2, game.height/2, {
-            font: '24px Josefin Slab',
+        var crystalsLabel = game.add.text(game.width/2, game.height/4.2, 'Crystals Collected: ' +
+            game.global.crystalsCollected, {font: '25px Josefin Slab', fill: '#ffffff'});
+        crystalsLabel.anchor.setTo(.5,.5);
+
+        var dustLabel = game.add.text(game.width/2, game.height/2.2-50, 'Dust Collected: ' + game.global.totalDustAccumulated + " kg", {font: '25px Josefin Slab', fill: '#ffffff'});
+        dustLabel.anchor.setTo(.5,.5);
+
+        var moneyLabel = game.add.text(game.width/2, game.height/2.2-100, 'Value of Metals Collected: $' + game.global.metalMoney, {font: '25px Josefin Slab', fill: '#ffffff'});
+        moneyLabel.anchor.setTo(.5,.5);
+
+        var nicknameLabel = game.add.text(game.width/2, game.height/2.2, 'Enter your name or sign in!', {font: '25px Josefin Slab', fill: '#ffffff'});
+        nicknameLabel.anchor.setTo(.5,.5);
+
+        this.nickname = game.add.inputField(game.width/2 - 80, game.height/2, {
+            font: '20px Josefin Slab',
             placeholder: 'Nickname',
-            placeholderColor: '#FFF',
+            placeholderColor: '#FFFFFF',
             type: PhaserInput.InputType.text
             });
-        this.nickname.anchor.setTo(.5,.5);
         this.nickname.focusOutOnEnter = true;
+        this.nickname.height = 25;
+        this.nickname.width = 250;
+        this.nickname.startFocus();
+
+        var playerName = this.nickname.value;
 
         this.guestStartButton = game.add.button(game.width/2.2, game.height/1.7, 'blueButton', this.start, this);
         this.guestStartButton.anchor.setTo(.5,.5);
@@ -48,6 +85,8 @@ var menuState = {
 
         this.helpButton = game.add.button(game.width*.95, 50, 'pinkButton', this.help, this);
         this.helpButton.anchor.setTo(.5,.5);
+
+        this.removeAdsButton = game.add.button(game.width*.95, game.height*.95, 'tealButton', this.removeAds, this);
 
         // Create a new Phaser keyboard variable; the up arrow key
         // When pressed, call 'start'
@@ -69,6 +108,7 @@ var menuState = {
 
         this.music.stop();
         game.state.start('play');
+        playState.nickname = this.nickname.value;
 
     },
 
@@ -90,6 +130,10 @@ var menuState = {
     gameMode: function(){
         this.music.stop();
         game.state.start('gameMode');
+    },
+
+    removeAds: function(){
+        console.log("Remove ads");
     }
 
 };
